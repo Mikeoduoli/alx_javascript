8f217ebca22233c9f41a35f986f1e1cf1b697f37
Mikeoduoli <mikeoduoli@gmail.com>
2023-09-16 22:58:11 +0300

    JavaScript Backend

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,26 +1,19 @@
 #!/usr/bin/node
-
 const myObject = {
     type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  function incr(number) {
-    return number + 1;
-  }
-  // Define the addMeMaybe function
-  function addMeMaybe(number, theFunction) {
-    const incrementedNumber = incr(number); // Increment the number
-    theFunction(incrementedNumber); // Call the function with the incremented number
-  }
-  // Export both functions so they can be used outside of this module (CommonJS style)
-  module.exports = {
-    addMeMaybe: addMeMaybe,
-    incr: incr
-  };
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
+    value: 12,
+    incr: function () {
+        this.value++;
+    }
+};
+const incr = 0;
+for (let i = 0; i < incr; i++) {
+    myObject.incr()
+}
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
