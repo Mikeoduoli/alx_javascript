0621ff8dac58342ae929a12ce7ffc34a89496663
Mikeoduoli <mikeoduoli@gmail.com>
2023-09-16 22:53:19 +0300

    JavaScript Backend

---
+++ javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+function addMeMaybe(number, theFunction) {
+    number++; // Increment the number
+    theFunction(number); // Call the function with the incremented number
+  }
+  
+  // Export the function so it can be used outside of this module (CommonJS style)
+  module.exports.addMeMaybe = addMeMaybe;
