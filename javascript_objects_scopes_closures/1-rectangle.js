3709a6abe2abcb35073af77551e4bdd42750c5ea
Mikeoduoli <mikeoduoli@gmail.com>
2023-09-17 19:52:22 +0300

    Objects, Scopes and Closures

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,40 +1,12 @@
-
+#!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-      if (w <= 0 || h <= 0) {
-        //To handle the case where width or height is not positive:
-        
-        return {};
-      } else if (h === undefined) {
-        //Case where height is not provided:
-        
-        this.width = w;
-        this.height = undefined;
-      } else {
-        // Normal case, both width and height are provided
-        this.width = w;
-        this.height = h;
-      }
+    width;
+    height;
+  
+    constructor (w, h) {
+      this.width = w;
+      this.height = h;
     }
   }
   
   module.exports = Rectangle;
-  
-  // usage:
-  
-  if (require.main === module) {
-    const r1 = new Rectangle(3, 3);
-    console.log(r1);
-    console.log(r1.width);
-    console.log(r1.height);
-  
-    const r2 = new Rectangle(3, -3);
-    console.log(r2);
-    console.log(r2.width);
-    console.log(r2.height);
-  
-    const r3 = new Rectangle(2);
-    console.log(r3);
-    console.log(r3.width);
-    console.log(r3.height);
-  }
