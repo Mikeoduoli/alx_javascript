147b58b6cb261163ab89e26cc6ff34053c836d56
Mikeoduoli <mikeoduoli@gmail.com>
2023-09-17 20:22:54 +0300

    Network

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,35 +1,10 @@
-#!/usr/bin/env node
+#!/usr/bin/node
+const request = require("request");
+const fs = require("fs");
 
-const request = require('request');
+const url = process.argv[2];
+const filepath = process.argv[3];
 
-
-if (process.argv.length < 3) {
-  console.error('Usage: node starwars_count.js <api_url>');
-  process.exit(1);
-}
-
-
-const apiUrl = process.argv[2];
-
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error('HTTP Status Code:', response.statusCode);
-  } else {
-    try {
-      
-      const filmsData = JSON.parse(body);
-
-      const moviesWithWedgeAntilles = filmsData.results.filter((movie) =>
-        movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
-      );
-
-      
-      console.log(moviesWithWedgeAntilles.length);
-    } catch (parseError) {
-      console.error('Error parsing JSON response:', parseError);
-    }
-  }
+request.get(url, function (error, response, body) {
+  fs.writeFileSync(filepath, response.body, { encoding: 'utf-8' });
 });
