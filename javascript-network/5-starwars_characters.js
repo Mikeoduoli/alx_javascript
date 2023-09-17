11554fbbdbc0713d8f8e23fe2ff45df907ad218d
Mikeoduoli <mikeoduoli@gmail.com>
2023-09-17 20:29:17 +0300

    Network

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -1,54 +1,47 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
 const request = require('request');
 
+ID = process.argv[2];
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/'+ID;
+const actors =[];
 
-if (process.argv.length !== 3) {
-  console.error('Usage: node starWarsCharacters.js <Movie-ID>');
-  process.exit(1);
-}
-
-const movieId = process.argv[2];
-
-
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-
-request(apiUrl, (error, response, body) => {
+request.get(apiUrl, (error, response, body) => {
   if (error) {
-    console.error('Error fetching movie data:', error);
-    process.exit(1);
+    console.error('Error:', error);
+    return;
   }
 
   if (response.statusCode !== 200) {
-    console.error('Failed to fetch movie data. Status code:', response.statusCode);
-    process.exit(1);
+    console.error('Request failed with status code:', response.statusCode);
+    return;
   }
 
   try {
-    const movieData = JSON.parse(body);
-
-    console.log(`Characters in "${movieData.title}":`);
-
-    
-    movieData.characters.forEach((characterUrl) => {
-      request(characterUrl, (charError, charResponse, charBody) => {
-        if (charError) {
-          console.error('Error fetching character data:', charError);
-          return;
-        }
-
-        if (charResponse.statusCode !== 200) {
-          console.error('Failed to fetch character data. Status code:', charResponse.statusCode);
-          return;
-        }
-
-        const characterData = JSON.parse(charBody);
-        console.log(characterData.name);
-      });
-    });
-  } catch (parseError) {
-    console.error('Error parsing movie data:', parseError);
-    process.exit(1);
+    const data = JSON.parse(body);
+    actors.push(data.characters);
+  } catch (e) {
+    console.error('Error parsing JSON:', e);
   }
+
+  actors[0].forEach(function (actor) {
+    request.get(actor, (error, response, body) => {
+        if (error) {
+          console.error('Error:', error);
+          return;
+        }
+      
+        if (response.statusCode !== 200) {
+          console.error('Request failed with status code:', response.statusCode);
+          return;
+        }
+      
+        try {
+          const data = JSON.parse(body);
+          console.log(data.name);
+        } catch (e) {
+          console.error('Error parsing JSON:', e);
+        }
+    })
+  })
 });
